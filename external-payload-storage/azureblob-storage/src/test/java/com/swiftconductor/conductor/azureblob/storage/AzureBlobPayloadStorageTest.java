53b7ea366951bd1ecaa1b6e0cb4e10a25705f6c9
vkantchev <vkantchev@users.noreply.github.com>
2024-01-08 18:27:35 -0800

    SC-61: Update package names in a few places

--- external-payload-storage/azureblob-storage/src/test/java/com/swiftconductor/conductor/azureblob/storage/AzureBlobPayloadStorageTest.java
+++ external-payload-storage/azureblob-storage/src/test/java/com/swiftconductor/conductor/azureblob/storage/AzureBlobPayloadStorageTest.java
@@ -21,6 +21,7 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import com.swiftconductor.conductor.azureblob.config.AzureBlobProperties;
+import com.swiftconductor.conductor.azureblob.storage.AzureBlobPayloadStorage;
 import com.swiftconductor.conductor.common.run.ExternalStorageLocation;
 import com.swiftconductor.conductor.common.utils.ExternalPayloadStorage;
 import com.swiftconductor.conductor.core.exception.NonTransientException;
